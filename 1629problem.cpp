5a9cb130fa70ab26c18be484427ab8859c0c9e3e
k9632441 <k9632441@gmail.com>
2020-06-01 00:15:01 +0900

    new file

---
+++ 1629problem.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+long long calc(long long a, long long b, long long c)
+{
+	if (b == 0)
+	{
+		return 1%c;
+	}
+	else if (b == 1)
+	{
+		return a%c;
+	}
+	else if (b % 2 == 0)
+	{
+		int temp = calc(a, b / 2, c);
+		return (temp%c * temp%c)%c;
+	}
+	else
+		return (a * calc(a, b - 1, c))%c;
+}
+
+int main()
+{
+	long long int a, b, c;
+	int result;
+	//double a, b, c;
+
+	scanf("%lld %lld %lld", &a,&b,&c);
+
+	a = a % c;
+	//b = b % c;
+	
+	result = calc(a, b, c);
+
+	printf("%d", result);
+	return 0;
+} 
